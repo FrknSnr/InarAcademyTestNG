4a9c1645f9bcb7a8f57caf7b7c27e833177052a6
furkansener <furkan@ortakapp.com>
2023-10-04 16:22:24 +0300

    updated

--- src/test/java/org/InarAcademy/pages/HomePage.java
+++ src/test/java/org/InarAcademy/pages/HomePage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import java.time.Duration;
 
 public class HomePage {
@@ -14,7 +13,6 @@ public class HomePage {
 
     @FindBy(id = "welcome-heading")
     private WebElement welcomeText;
-
     @FindBy(id = "order-tab")
     private WebElement orderTab;
     @FindBy(id = "view-orders-tab")

78ac9449991e8668237df7918d5373b1ebf05508
furkansener <furkan@ortakapp.com>
2024-02-17 14:45:11 +0300

    latest

--- src/test/java/org/InarAcademy/testSuites/weborder/LoginTests.java
+++ src/test/java/org/InarAcademy/testSuites/weborder/LoginTests.java
@@ -5,15 +5,11 @@ import org.InarAcademy.pages.weborder.HomePage;
 import org.InarAcademy.pages.weborder.WeborderLoginPage;
 import org.InarAcademy.testSuites.TestBase;
 import org.InarAcademy.dataProvider.PasswordData;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
-
-    private static final Logger log = (Logger) LogManager.getLogger(LoginTests.class);
 
     WeborderLoginPage weborderLoginPage;
 
@@ -27,23 +23,15 @@ public class LoginTests extends TestBase {
         Assert.assertTrue(weborderLoginPage.isUsernameInputDisplayed());
         Assert.assertTrue(weborderLoginPage.isPasswordInputDisplayed());
         Assert.assertTrue(weborderLoginPage.isLoginButtonDisplayed());
-
-        //we can use log methods to see the results as logs in the console
-        log.info("This is an information message");
-        log.debug("This is a debug message");
-
         Assert.assertEquals(weborderLoginPage.getDescriptionText(), "In order to log in, use the following information:");
         Assert.assertEquals(weborderLoginPage.getUsernameInfoText(), "Username: Inar");
         Assert.assertEquals(weborderLoginPage.getPasswordInfoText(), "Password: Academy");
     }
 
-    @Test(groups = {"smoke"})
+    /*@Test(groups = {"smoke"})
     public void loginWithValidCredentials() {
         HomePage homePage = weborderLoginPage.login("Inar", "Academy");
         Assert.assertEquals(homePage.getWelcomeText(), "Welcome, Inar!");
-
-        //we can use log methods to see the results as logs in the console
-        log.info("Login with valid credentials test is passed");
     }
 
     @Test(dataProvider = "usernameData", dataProviderClass = UsernameData.class)
@@ -56,5 +44,5 @@ public class LoginTests extends TestBase {
     public void loginWithInvalidPassword(String username, String password, String alertMessage) {
         weborderLoginPage.login(username, password);
         Assert.assertEquals(weborderLoginPage.getPasswordAlertMessage(), alertMessage);
-    }
+    }*/
 }
--- src/test/java/org/InarAcademy/utils/ReusableMethods.java
+++ src/test/java/org/InarAcademy/utils/ReusableMethods.java
@@ -3,6 +3,7 @@ package org.InarAcademy.utils;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
